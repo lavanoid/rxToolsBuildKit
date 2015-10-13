2bb16ac8eed6cec752408448a236ef349fd4241b
Jack Davies <lavanoid@gmail.com>
2015-10-13 15:09:34 +0100

    Added verification

--- stuff/download_aria2.bat
+++ stuff/download_aria2.bat
@@ -7,5 +7,18 @@ if not exist "%INS_DATA%\stuff\download_vbs.bat" (
   goto :EOF
 )
 call "%INS_DATA%\stuff\download_vbs.bat" "https://dl.dropbox.com/s/yr0z9boob1w5elx/aria2c.exe" "%INS_DATA%\stuff\aria2c.exe"
-if not exist "%INS_DATA%\stuff\aria2c.exe" echo Failed to download aria2c!
-set error=true
+if not exist "%INS_DATA%\stuff\aria2c.exe" (
+  echo Failed to download aria2c!
+  set error=true
+)
+if not exist "%INS_DATA%\stuff\verify_file.bat" (
+  echo **ERROR* Could not verify the file! "%INS_DATA%\stuff\verify_file.bat" is missing!
+  color c
+  goto :EOF
+)
+"%INS_DATA%\stuff\verify_file.bat" "%INS_DATA%\stuff\aria2c.exe" 4456448
+if "%error%"=="true" (
+  echo **ERROR** File has failed verification. Cannot continue.
+  color c
+  goto :EOF
+)
